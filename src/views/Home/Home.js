71f8f9f692ab74a03ac30515b21d1dfabe64c513
samuel <spbsamuel@gmail.com>
2018-01-31 17:24:09 +0800

    scaffolds main ui components

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,11 +1,54 @@
 import React from 'react'
-
-import Counter from 'connected_components/Counter'
+import cls from './Home.scss'
+import cx from 'classnames'
+import SearchIcon from 'mdi-react/MagnifyIcon'
+import HeartIcon from 'mdi-react/HeartIcon'
 
 function Home() {
   return (
-    <div>
-      <Counter/>
+    <div className={cls.Home}>
+      <div className={cls.SearchGroup}>
+        <FavouritesBar/>
+        <SearchBar/>
+        <SearchResults/>
+      </div>
+      <div className={cls.InfoPanelWrapper}>
+        <InfoPanel />
+      </div>
+    </div>
+  )
+}
+
+function SearchBar() {
+  return (
+    <div className={cls.SearchBar}>
+      <SearchIcon/>
+      <input className={cls.SearchInput}/>
+    </div>
+  )
+}
+
+function SearchResults() {
+  return (
+    <div className={cls.SearchResults}>
+      SearchResults
+    </div>
+  )
+}
+
+function InfoPanel() {
+  return (
+    <div className={cls.InfoPanel}>
+      InfoPanel
+    </div>
+  )
+}
+
+function FavouritesBar() {
+  return (
+    <div className={cls.FavouritesBar}>
+      <h3>My Favourites</h3>
+      <HeartIcon/>
     </div>
   )
 }
